02a9d19154f3488b0631fa1b6c799fe65caf3906
Ravi Shanmugam <ravi@avizia.com>
2017-09-08 21:26:51 -0400

    refactor

--- static/app.js
+++ static/app.js
@@ -8,5 +8,10 @@
           }
      ]);
 
-     
+     angular.module('walmart').filter('startFrom', function() {
+          return function(input, start) {
+              start = +start; //parse to int
+              return input.slice(start);
+          }
+      });
 })();
--- static/app/product-detail/product-detail-controller.js
+++ static/app/product-detail/product-detail-controller.js
@@ -3,9 +3,24 @@
 (function () {
      angular.module('walmart').controller('ProductDetailController', ProductDetailController);
 
-     function ProductDetailController($stateParams, ProductsService, $state, _, $q) {
+     function ProductDetailController($stateParams, ProductsService, $state, _, $q, $timeout) {
           var self = this;
           self.itemId = $stateParams.itemId
+          self.currentList = 1;
+          self.listSize = 5;
+          self.nextSet = nextSet;
+          self.previousSet = previousSet;
+
+          function nextSet(){
+               self.currentList++;
+              
+          }
+
+          function previousSet(){
+               self.currentList--;
+               
+          }
+          
           function init(){
                ProductsService.getItemById(self.itemId)
                .then((result) => {
